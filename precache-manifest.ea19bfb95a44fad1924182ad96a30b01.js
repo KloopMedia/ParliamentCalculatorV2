34efe106d8e22f7c8b823f308e33d318f3997878
xakim <xakim@pop-os.localdomain>
2020-10-20 00:59:33 +0600

    Updates

---
+++ precache-manifest.ea19bfb95a44fad1924182ad96a30b01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6ef797fc9c6e3e248eb84f4422ed66",
+    "url": "/ParliamentCalculatorV2/index.html"
+  },
+  {
+    "revision": "0a141b7e9a27a6ff710f",
+    "url": "/ParliamentCalculatorV2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a62ddfa777a41e025867",
+    "url": "/ParliamentCalculatorV2/static/js/2.819eef6f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ParliamentCalculatorV2/static/js/2.819eef6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a141b7e9a27a6ff710f",
+    "url": "/ParliamentCalculatorV2/static/js/main.f0c3d1d4.chunk.js"
+  },
+  {
+    "revision": "5321c86534b83f0e03ab",
+    "url": "/ParliamentCalculatorV2/static/js/runtime-main.992f1da6.js"
+  },
+  {
+    "revision": "42ccabd34e43cc9f80495d75581c8096",
+    "url": "/ParliamentCalculatorV2/static/media/PARTIES_RESULTS_REACT_LEVEL_ONE.42ccabd3.csv"
+  }
+]);
